ddbd63bcdb27da4ba1a90a40124516f9dc6f0b47
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-06-19 16:24:15 -0300

    Full API

--- src/main/java/com/system_management_student/system_management_student/controller/StudentController.java
+++ src/main/java/com/system_management_student/system_management_student/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.system_management_student.system_management_student.controller;
 
+import com.system_management_student.system_management_student.modal.dto.ViewData.DataStudentsDto;
 import com.system_management_student.system_management_student.modal.dto.ViewData.DataUsersDto;
+import com.system_management_student.system_management_student.services.insert.AddNotaStudent;
 import com.system_management_student.system_management_student.services.insert.AddStudentServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class StudentController {
     private final AddStudentServices addStudentServices;
+    private final AddNotaStudent addNotaStudent;
 
-    public StudentController(AddStudentServices addStudentServices) {
+    public StudentController(AddStudentServices addStudentServices, AddNotaStudent addNotaStudent) {
         this.addStudentServices = addStudentServices;
+        this.addNotaStudent = addNotaStudent;
     }
 
     @PostMapping("/add/student")
@@ -22,6 +26,17 @@ public class StudentController {
         try{
             addStudentServices.insertStudent(dto);
             return ResponseEntity.ok().body("Student added successfully");
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("An error occured while adding student");
+        }
+    }
+
+    @PostMapping("/add/student/nota")
+    public ResponseEntity<String> addNota(@RequestBody DataStudentsDto dto){
+        try{
+            addNotaStudent.insertNotaStudent(dto);
+            return ResponseEntity.ok().body("Nota added successfully");
         } catch (Exception e){
             e.printStackTrace();
             return ResponseEntity.internalServerError().body("An error occured while adding student");
--- src/main/java/com/system_management_student/system_management_student/modal/repository/DataStudentsRepository.java
+++ src/main/java/com/system_management_student/system_management_student/modal/repository/DataStudentsRepository.java
@@ -2,6 +2,11 @@ package com.system_management_student.system_management_student.modal.repository
 
 import com.system_management_student.system_management_student.modal.entity.DataStudents;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DataStudentsRepository extends JpaRepository<DataStudents, Integer> {
+import java.util.Optional;
+
+@Repository
+public interface DataStudentsRepository extends JpaRepository<DataStudents, Long> {
+    Optional<DataStudents> findByStudentIdAndBimestreAndAno(Long studentId, Integer bimestre, Integer ano);
 }
