8c312667a0d183f0e62779c84ee8cdb338d6f57c
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-06-21 18:46:33 -0300

    feat : configuration the Spring Security and adding bean of BCryptPasswordEncoder

---
+++ src/main/java/com/system_management_student/system_management_student/Configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.system_management_student.system_management_student.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
