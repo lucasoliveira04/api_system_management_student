4c6954f42227fc13621b93580aa076806dee9ab2
Lucas <camposdlucasoli@gmail.com>
2024-07-22 17:21:12 -0300

    configuração CORS

--- src/main/java/com/system_management_student/system_management_student/exception/CustomExceptions.java
+++ src/main/java/com/system_management_student/system_management_student/exception/CustomExceptions.java
@@ -1,6 +1,7 @@
 package com.system_management_student.system_management_student.exception;
 
 public class CustomExceptions {
+
     public static class InvalidFieldException extends RuntimeException {
         public InvalidFieldException(String message) {
             super(message);
